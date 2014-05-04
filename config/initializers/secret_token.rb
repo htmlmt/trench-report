1c263bec9177763608715d235f3406bb46741875
Michael Todd <mikejtodd@gmail.com>
2014-05-04 18:29:09 -0500

    Initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrenchReport::Application.config.secret_token = 'b91840f97ddd654488c19d6143984a50fb89d5108df79e3cc3627ac4a9720b85cfed3ad1b0817f50bda0d1bb7ebbf87d66b94c1786b1246fe8933c509ad73da3'
